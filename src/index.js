9ced4b953769f2ebc564b110f272982703c3ca8e
Christina Lambiase <christina.lambiase@gmail.com>
2023-06-22 11:22:24 -0700

    complete all deliverables

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@
 //[x] write a render function to render the ramen details to the div by selecting each of the HTML elements in global scope, then in the render function setting their text content equal to the keys.data from the json object
 //[x] adding a click event listener to the image variable inside the render images function that passes in a click, and calls the render dish function
 
+//Third Deliverable, I need to:
+//[x] define variables in global scope for form and form inputs
+//[x] add a submit event listener to the form that passes in a callback to render the new dish to the DOM
+//[x] render the new dish inside the event listener, setting the content of the elements equal to the values submitted on the form
+
+
 //Global variables
 
 const URL = "http://localhost:3000/ramens"
@@ -28,6 +34,12 @@ const ramenName = document.querySelector('.name')
 const restaurant = document.querySelector('.restaurant')
 const rating = document.querySelector("#rating-display")
 const comment = document.querySelector("#comment-display")
+const ramenForm = document.querySelector('#new-ramen')
+const newImage = document.querySelector("#new-image")
+const newName = document.querySelector("#new-name")
+const newRestaurant = document.querySelector("#new-restaurant")
+const newRating = document.querySelector("#new-rating")
+const newComment = document.querySelector("#new-comment")
 
 
 //Fetch function
@@ -63,3 +75,15 @@ function renderDish(ramens) {
     comment.textContent = ramens.comment
 
 }
+
+
+// Event Listeners
+
+ramenForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    detailPic.src = newImage.value
+    ramenName.textContent = newName.value
+    restaurant.textContent = newRestaurant.value
+    rating.textContent = newRating.value
+    comment.textContent = newComment.value
+})
